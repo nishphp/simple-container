07066d945e19adcc9b2865ef3e8f57dc5f528d5f
Satoshi Nishimura <nishim314@gmail.com>
2018-10-23 14:49:23 +0900

    initial commit

---
+++ src/Container.php
@@ -0,0 +1,203 @@
+<?php
+declare(strict_types=1);
+
+namespace Nish\Container;
+
+use Psr\Container\ContainerInterface;
+use Psr\Container\NotFoundExceptionInterface;
+use Psr\Container\ContainerExceptionInterface;
+use Exception;
+use ReflectionClass;
+use ReflectionMethod;
+use ReflectionException;
+
+class Container implements ContainerInterface
+{
+    /** @var array */
+    protected $components = [];
+    /** @var array */
+    protected $factories = [];
+
+    protected function __construct(){}
+
+    /**
+     * @param string $id component name
+     * @return mixed
+     * @throws ContainerExceptionInterface component initialize error
+     * @throws NotFoundExceptionInterface component not found
+     */
+    public function get($id)
+    {
+        if (isset($this->components[$id]))
+            return $this->components[$id];
+
+        if (isset($this->factories[$id])){
+            try {
+                $f = $this->factories[$id];
+                return $f($this);
+            }catch (\Throwable $e){
+                throw new class($id . ' factory error.', 0, $e)
+                    extends \Exception
+                    implements ContainerExceptionInterface{};
+            }
+        }
+
+        if (class_exists($id))
+            return $this->buildByReflection($id);
+
+        throw new class($id . ' not found')
+            extends Exception
+            implements NotFoundExceptionInterface{};
+    }
+
+    /**
+     * @param string $id
+     */
+    public function has($id)
+    {
+        return isset($this->components[$id]) || isset($this->factories[$id]);
+    }
+
+    public function set(string $id, $component): self
+    {
+        $this->components[$id] = $component;
+        return $this;
+    }
+
+    public function setFactory(string $id, callable $factory): self
+    {
+        $this->factories[$id] = $factory;
+        return $this;
+    }
+
+    public function clear(): self
+    {
+        $this->components = [];
+        $this->factories = [];
+        return $this;
+    }
+
+
+    public static function getInstance(): self
+    {
+        static $instance;
+        if ($instance === null)
+            $instance = new self();
+        return $instance;
+    }
+
+    /**
+     * alias of get
+     * @return mixed
+     */
+    public static function gets(string $id)
+    {
+        return self::getInstance()->get($id);
+    }
+
+    // alias
+    /**
+     * @return mixed|null
+     */
+    public static function mayget($id)
+    {
+        $container = self::getInstance();
+        if ($container->has($id))
+            return $container->get($id);
+        else
+            return null;
+    }
+
+    //
+    //
+    // Auto Wiring
+    //
+    //
+    /**
+     * @param string|object $class class name or object
+     * @param string $name method name
+     * @param array $callParams additional parameters
+     */
+    public function call($class, string $name, array $callParams = [])
+    {
+        try {
+            $refMethod = new ReflectionMethod($class, $name);
+        }catch (ReflectionException $e){
+            throw new Exception('auto parameter setting error', 0, $e);
+        }
+
+        $params = $this->getMethodParams($refMethod, $class, $callParams);
+        /** @var callable */
+        $method = [$class, $name];
+        return $method(...$params);
+    }
+
+    protected function buildByReflection($name)
+    {
+        if (!class_exists($name))
+            throw new Exception("class name [$name] not found");
+
+        try {
+            $refClass = new ReflectionClass($name);
+            $refMethod = $refClass->getConstructor();
+            if ($refMethod !== null)
+                $params = $this->getMethodParams($refMethod, $name);
+            else
+                $params = array();
+        }catch (ReflectionException $e){
+            throw new Exception("build error", 0, $e);
+        }
+
+        $obj = $refClass->newInstanceArgs($params);
+        $this->set($name, $obj);
+        return $obj;
+    }
+
+    protected function getMethodParams($refMethod, $class, $callParams = [])
+    {
+        if (is_string($class))
+            $className = $class;
+        else
+            $className = get_class($class);
+        $params = array();
+        $refParams = $refMethod->getParameters();
+        foreach ($refParams as $refParam){
+            $paramName = $refParam->getName();
+            if (isset($callParams[$paramName]))
+                $params[] = $callParams[$paramName];
+            else
+                $params[] = $this->getParam($refParam, $className);
+        }
+        return $params;
+    }
+
+    protected function getParam($refParam, $className)
+    {
+
+        $methodName = $refParam->getDeclaringFunction()->getName();
+        $paramName = $refParam->getName();
+        $key = "$className#$methodName.$paramName";
+        if ($this->has($key))
+            return $this->get($key);
+
+        $refClass = $refParam->getClass();
+        if (!$refClass){
+            if (!$refParam->isOptional())
+                throw new Exception('can not parse ' .
+                                    $className . '#' . $methodName);
+
+            return $refParam->getDefaultValue();
+        }
+
+        $name = $refClass->getName();
+
+        try {
+            return $this->get($name);
+        }catch (Exception $e){
+            if (!$refParam->isOptional())
+                throw $e;
+
+            return $refParam->getDefaultValue();
+        }
+    }
+}
---
+++ test/ContainerTest.php
@@ -0,0 +1,230 @@
+<?php
+declare(strict_types=1);
+
+namespace Nish\Container;
+
+use PHPUnit\Framework\TestCase;
+use Psr\Container\ContainerInterface;
+use Psr\Container\NotFoundExceptionInterface;
+use Psr\Container\ContainerExceptionInterface;
+use Exception;
+use Error;
+use RuntimeException;
+use stdClass;
+use ArrayObject;
+
+class Foo
+{
+    public $bar;
+    public function __construct(Bar $bar){ $this->bar = $bar; }
+    public function method(Bar $bar, Baz $baz){
+        return [$bar, $baz];
+    }
+    public function staticMethod(Bar $bar, Baz $baz){
+        return [$baz, $bar];
+    }
+
+    public function methodWithDefault(string $arg = 'def')
+    {
+        return $arg;
+    }
+
+    public function notResolve(Bar $arg = null)
+    {
+        return $arg;
+    }
+
+    public function notFound(Fooo $arg)
+    {
+        return $arg;
+    }
+}
+class Bar {}
+class Baz {}
+
+
+final class ContainerTest extends TestCase
+{
+    public function tearDown()
+    {
+        Container::getInstance()->clear();
+    }
+
+    public function testPrivateConstructor()
+    {
+        $this->expectException(Error::class);
+        $c = new Container();
+    }
+
+    public function testGetInstance()
+    {
+        $c = Container::getInstance();
+        $this->assertInstanceOf(Container::class, $c);
+        $this->assertInstanceOf(ContainerInterface::class, $c);
+    }
+
+    public function testSetAndGet()
+    {
+        $c = Container::getInstance();
+        $obj = new stdClass();
+        $obj->foo = 'bar';
+        $c->set(stdClass::class, $obj);
+        $obj2 = $c->get(stdClass::class);
+
+        $this->assertSame($obj, $obj2);
+
+        $obj3 = Container::gets(stdClass::class);
+        $this->assertInstanceOf(stdClass::class, $obj3);
+    }
+
+    public function testMayGet()
+    {
+        $c = Container::getInstance();
+
+        $obj = Container::mayget(stdClass::class);
+        $this->assertNull($obj);
+
+        $obj = new stdClass();
+        $obj->foo = 'bar';
+        $c->set(stdClass::class, $obj);
+
+        $obj = Container::mayget(stdClass::class);
+        $this->assertInstanceOf(stdClass::class, $obj);
+    }
+
+    public function testFactory()
+    {
+        $c = Container::getInstance();
+        $c->setFactory(stdClass::class, function($c){
+            $obj = new stdClass();
+            $obj->foo = 'bar';
+            return $obj;
+        });
+
+        $a = $c->get(stdClass::class);
+        $this->assertInstanceOf(stdClass::class, $a);
+        $b = $c->get(stdClass::class);
+        $this->assertInstanceOf(stdClass::class, $b);
+        $this->assertNotSame($a, $b);
+    }
+
+    public function testFactoryRegister()
+    {
+        $c = Container::getInstance();
+        $c->setFactory(stdClass::class, function($c){
+            $obj = new stdClass();
+            $obj->foo = 'bar';
+            $c->set(stdClass::class, $obj);
+            return $obj;
+        });
+
+        $a = $c->get(stdClass::class);
+        $b = $c->get(stdClass::class);
+        $this->assertSame($a, $b);
+    }
+
+    public function testFactoryException()
+    {
+        $this->expectException(ContainerExceptionInterface::class);
+
+        $c = Container::getInstance();
+        $c->setFactory(stdClass::class, function($c){
+            $obj = new NotFound();
+            return $obj;
+        });
+        $a = $c->get(stdClass::class);
+    }
+
+    public function testAutoWiring()
+    {
+        $c = Container::getInstance();
+        $a = $c->get(Foo::class);
+        $this->assertInstanceOf(Foo::class, $a);
+        $this->assertInstanceOf(Bar::class, $a->bar);
+    }
+
+    public function testAutoWiringWithParameter()
+    {
+        $c = Container::getInstance();
+        $c->set('ArrayObject#__construct.input', ['foo' => 'bar']);
+        $c->set('ArrayObject#__construct.flags', 0);
+        $c->set('ArrayObject#__construct.iterator_class', 'ArrayIterator');
+        $a = $c->get(ArrayObject::class);
+        $this->assertEquals($a['foo'], 'bar');
+    }
+
+    public function testCallAutoWiring()
+    {
+        $c = Container::getInstance();
+
+        list($a, $b) = $c->call(Foo::class, 'staticMethod');
+        $this->assertInstanceOf(Baz::class, $a);
+        $this->assertInstanceOf(Bar::class, $b);
+
+        $foo = new Foo(new Bar());
+        list($a, $b) = $c->call($foo, 'method');
+        $this->assertInstanceOf(Bar::class, $a);
+        $this->assertInstanceOf(Baz::class, $b);
+    }
+
+    public function testCallMethodWithDefault()
+    {
+        $c = Container::getInstance();
+        $foo = new Foo(new Bar());
+        $ret = $c->call($foo, 'methodWithDefault');
+        $this->assertEquals('def', $ret);
+    }
+
+    public function testOverrideDefaultArgument()
+    {
+        $c = Container::getInstance();
+        $c->set(Foo::class . '#methodWithDefault.arg', 'override');
+
+        $foo = new Foo(new Bar());
+        $ret = $c->call($foo, 'methodWithDefault');
+        $this->assertEquals('override', $ret);
+    }
+
+    public function testNotResolveArgument()
+    {
+        $c = Container::getInstance();
+        $c->setFactory(Bar::class, function($c){
+            throw new RuntimeException('deep error');
+        });
+
+        $foo = new Foo(new Bar());
+        $ret = $c->call($foo, 'notResolve');
+        $this->assertNull($ret);
+    }
+
+    public function testCallException()
+    {
+        // auto wiring
+        // not ContainerExceptionInterface
+        $this->expectException(Exception::class);
+
+        $c = Container::getInstance();
+        $c->call(Foo::class, 'dummyMethod');
+    }
+
+    public function testExceptionDeep()
+    {
+        $this->expectException(ContainerExceptionInterface::class);
+
+        $c = Container::getInstance();
+        $c->setFactory(Bar::class, function($c){
+            throw new RuntimeException('deep error');
+        });
+        // constructor is not allowed null argument
+        $c->get(Foo::class);
+    }
+
+    public function testCallNotFoundArgumentClass()
+    {
+        $this->expectException(Exception::class);
+
+        $c = Container::getInstance();
+        $c->call(Foo::class, 'notFound');
+    }
+
+}
